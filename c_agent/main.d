84e5f57e86dc5afe53b21b71f7bb8b76c81e696d
nahueldepp <nahueldeppen45@gmail.com>
2026-06-23 14:12:03 +0000

    a

---
+++ c_agent/main.d
@@ -0,0 +1,5 @@
+c_agent/main.o: c_agent/main.c c_agent/resource_manager.h \
+ c_agent/server.h c_agent/coordinador_jobs.h
+c_agent/resource_manager.h:
+c_agent/server.h:
+c_agent/coordinador_jobs.h:
---
+++ c_agent/server.d
@@ -0,0 +1,5 @@
+c_agent/server.o: c_agent/server.c c_agent/coordinador_jobs.h \
+ c_agent/resource_manager.h c_agent/server.h
+c_agent/coordinador_jobs.h:
+c_agent/resource_manager.h:
+c_agent/server.h:
